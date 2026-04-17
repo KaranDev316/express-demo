de322d9239e8201d81885469907d67d7f138e75d
Alfred Mtambalika <karanlicka@gmail.com>
2026-04-17 13:48:21 +0530

    complete exercise

--- exercise.js
+++ exercise.js
@@ -1,16 +1,20 @@
 
-getCustomer(1, (customer) => {
-  console.log('Customer: ', customer);
-  if (customer.isGold) {
-    getTopMovies((movies) => {
+async function notifyCustomer() {
+  try {
+    const customer = await getCustomer(1);
+    console.log('Customer: ', customer);
+    if (customer.isGold) {
+      const movies = await getTopMovies();
       console.log('Top movies: ', movies);
-      sendEmail(customer.email, movies, () => {
-        console.log('Email sent...')
-      });
-    });
+      await sendEmail(customer.email, movies);
+      console.log('Email sent...');
+    }
+  } catch (err) {
+    console.log(err);
   }
-});
+}
 
+notifyCustomer();
 function getCustomer(id) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -32,7 +36,7 @@ function getTopMovies() {
 });
 }
 
-function sendEmail(email, movies, callback) {
+function sendEmail(email, movies) {
  return new Promise((resolve, reject) => {
    setTimeout(() => {
      resolve();
