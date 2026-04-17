747470a48699ae8e72a3a827184e0024692ba7e6
Alfred Mtambalika <karanlicka@gmail.com>
2026-04-17 12:01:24 +0530

    Getting repo name on async function

--- callback.js
+++ callback.js
@@ -1,5 +1,7 @@
 getUser(1, (user) => {
     console.log('User:', user);
+    const repos = getRepositories(user);
+    console.log('Repos:', repos);
 });
 
 function getUser(id, callback) {
@@ -8,3 +10,9 @@ function getUser(id, callback) {
     callback({ id: id, name: 'Alfred' });
   }, 2000);
 }
+
+function getRepositories(username) {
+    return ['repo1', 'repo2', 'repo3'];
+}
+  
+   
