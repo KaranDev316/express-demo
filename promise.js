2828b98057d2b5aeb37beb5d2c5aedf528139d40
Alfred Mtambalika <karanlicka@gmail.com>
2026-04-17 12:56:14 +0530

    using promises inside function

--- promise.js
+++ promise.js
@@ -1,35 +1,39 @@
+
 console.log('Before');
-getUser(1, (user) => {
-  getRepositories(user.gitHubUsername, (repos) => {
-    getCommits(repos[0], (commits) => {
-      console.log(commits);
-    })
-  })
-});
+
 console.log('After');
 
-function getUser(id, callback) {
+getUser(1)
+  .then(user => getRepositories(user.gitHubUsername))
+  .then(repos => getCommits(repos[0]))
+  .then(commits => console.log(commits))
+  .catch(err => console.log(err));  
+
+function getUser(id) {
+    return new Promise((resolve, reject) => {
   setTimeout(() => {
     console.log('Reading a user from a database...');
-    callback({ id: id, gitHubUsername: 'mosh' });
+    resolve({ id: id, gitHubUsername: 'mosh' });
   }, 2000);
+}   
+);
 }
 
-function getRepositories(username, callback) {
+function getRepositories(username) {
+    return new Promise((resolve, reject) => {
   setTimeout(() => {
     console.log('Calling GitHub API...');
-    callback(['repo1', 'repo2', 'repo3']);
+    resolve(['repo1', 'repo2', 'repo3']);
   }, 2000);
+});
 }
 
-
-const getCommits = new Promise((resolve, reject) => {
+function getCommits(repo) {
+    return new Promise((resolve, reject) => {
   setTimeout(() => {
     console.log('Calling GitHub API...');
     resolve(['commit']);
   }, 2000);
-});
-
-getCommits
-  .then(commits => console.log(commits))
-  .catch(err => console.log(err));
+});   
+}
+        
